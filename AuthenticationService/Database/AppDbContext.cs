3f0b397c576f40de5e48a01b3ddabaa5f03c34ab
TymianekPL <tymi@tymi.org>
2025-11-27 13:29:24 +0100

    SLN updates

---
+++ AuthenticationService/Database/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace AuthenticationService.Database
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = ();
+    }
+}
---
+++ AuthenticationService/Database/User.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationService.Database
+{
+    public sealed class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+    }
+}
